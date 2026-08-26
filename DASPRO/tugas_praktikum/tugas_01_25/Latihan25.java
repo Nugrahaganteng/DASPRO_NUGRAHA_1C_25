c00e3620760c7743cf7563d1b01397e21daf2c2f
NugrahaAlgeo <157438272+Nugrahaganteng@users.noreply.github.com>
2026-08-26 19:42:30 +0700

    Add files via upload

---
+++ DASPRO/tugas_praktikum/tugas_01_25/Latihan25.java
@@ -0,0 +1,10 @@
+public class Latihan25 {
+
+    public static void main(String[] args) {
+        System.out.println("Halo, nama saya adalah Nugraha algeio firizki subarji");
+  //  System.out.print("Belajar Java itu mudah!");
+    // System.out.print("\nBelajar Java itu mudah!");
+        System.out.println("Belajar Java itu mudah!");
+        System.out.println("Semakin sering mencoba, semakin jago.");
+    }
+}
